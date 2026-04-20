4a640af76c2c6ce51e1ffe6b03f7ff10dd8f0703
ryan <ryancowan55555@gmail.com>
2026-04-20 13:29:48 +0800

    added ACO to main.py

--- main.py
+++ main.py
@@ -6,21 +6,29 @@ start = time.time()
 
 if '-g' in sys.argv:
     print('Genetic Algorithm')
-    print(genetic_algorithm(100, 100))
+    print(genetic_algorithm(500, 500))
 
 elif '-g+' in sys.argv:
     print('Genetic Algorithm with graph')
-    print(genetic_algorithm(100, 100, graph='genetic_graph.png'))
+    print(genetic_algorithm(500, 500, graph='genetic_graph.png'))
     print("graph is located at 'genetic_graph.png'")
 
 elif '-p' in sys.argv:
     print('Particle Swarm')
     print(optimiseParticleSwarm())
 
+elif '-a' in sys.argv:
+    print('Ant colony optimisation')
+    import ACO_ASS1
+
 elif '-t' in sys.argv:
     print('Testing')
-    print(f'   genetic-algorithm   {genetic_algorithm(100, 100, graph='genetic_graph.png').getCost()}')
+    print(f'   genetic-algorithm   {genetic_algorithm(500, 500, graph='genetic_graph.png').getCost()}')
     print(f'   particle-swarm      {optimiseParticleSwarm()[1]}')
+    print(f'   ant-colony         :')
+    import ACO_ASS1
+    print()
+    print('No Runtime Errors')
     print()
 
 
@@ -30,10 +38,11 @@ else:
     print('   -g   genetic algorithm')
     print('   -g+  genetic algorithm with graph output (slower)')
     print('   -p   particle swarm')
+    print('   -a   ant colony optimisation')
     print()
     print('   -t   test algorithms')
     print()
-    print('eg: "python main.py -g"')
+    print('eg: "python main.py -t"')
     exit()
 
 elapsed = time.time() - start
